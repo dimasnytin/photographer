07786dc3626ced8100ae56691a459cf97772b6b0
Dima Snytin <dimasnytin@gmail.com>
2022-10-23 15:24:44 +0300

    add navbar js code and navmenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,45 @@
-'use strict';
+// add Event on multiple elment
 
-
-
-// добавить событие на несколько элементов
-const addEventOnElements = function(elements, evenType, callback) {
-  for(let i = 0; i < elements.lenght; i++){
-    elements[i].addEventListener(evenType, callback);
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
   }
 }
 
-// PREOLOADING ЗАГРУЗЧИК
+
+
+// PRELOADING
+
 const loadingElement = document.querySelector("[data-loading]");
 
 window.addEventListener("load", function () {
   loadingElement.classList.add("loaded");
   document.body.classList.remove("active");
 });
+
+
+
+// MOBILE NAV TOGGLE
+
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll("[data-nav-toggler]"),
+  document.querySelectorAll("[data-nav-link]"),
+  document.querySelector("[data-navbar]"),
+  document.querySelector("[data-overlay]")
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", closeNav);
